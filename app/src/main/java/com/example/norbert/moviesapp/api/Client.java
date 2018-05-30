206fafe7c83c2f59cc080e5dc55683bc7546e76a
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-30 18:50:53 +0200

    Przygotowanie API. Utworzenie Client API

--- app/src/main/java/com/example/norbert/moviesapp/api/Client.java
+++ app/src/main/java/com/example/norbert/moviesapp/api/Client.java
@@ -1,8 +1,24 @@
 package com.example.norbert.moviesapp.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Norbert on 2018-05-30.
  */
 
 public class Client {
+
+    public static  final String BASE_URL = "http://api.themoviedb.org/3/";
+    public static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
